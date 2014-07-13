354683f1bfd8acea91f7e6bda1792ed7907187a1
Stephanie <sathomas9@gmail.com>
2014-07-13 12:07:51 -0700

    Plot 1 R code file

---
+++ R code files/plot1.R
@@ -0,0 +1,21 @@
+##Script used to create plot1.png
+##Read in and clean the data.
+##The data file has already been saved and unzipped to the working directory. 
+setwd("~/Documents/Coursera/Class 4")
+household_power_consumption <- read.csv("~/Documents/Coursera/Class 4/household_power_consumption.txt", 
+               sep=";", na.strings="?", stringsAsFactors=FALSE)
+View(household_power_consumption)
+
+power1 <- subset(household_power_consumption, Date == "1/2/2007")
+power2 <- subset(household_power_consumption, Date == "2/2/2007")
+powerbind <- rbind(power1, power2)
+DateTime <- with(powerbind, paste(Date, Time, sep=" "))
+DateandTime <- strptime(DateTime, "%d/%m/%Y %H:%M:%S")
+
+bind <- cbind(DateandTime, powerbind)
+
+##Create plot 1
+x <- as.numeric(bind$Global_active_power)
+hist(x, main="Global Active Power",
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", freq=TRUE, 
+              ylim=c(0,1200), col="red", breaks = 11, seq(from=0, to=8, by=2), cex.axis=0.8, cex.lab=0.9)
